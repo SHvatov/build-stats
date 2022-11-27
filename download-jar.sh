2e6fbaaed337ebb541576c11d430b2e7c5db104d
Sergey Hvatov <ishvatov@gmail.com>
2022-11-27 23:07:49 +0400

    Some improvements to the CI/CD

--- download-jar.sh
+++ download-jar.sh
@@ -2,4 +2,5 @@ curl -s https://api.github.com/repos/SHvatov/build-stats/releases/latest \
 | grep "browser_download_url.*.jar" \
 | cut -d : -f 2,3 \
 | tr -d \" \
+| grep "<module-name>" \
 | wget -qi -
